9b6dc32373734fda186f90d9bc5c51fae8c503fd
YuukaDev <vule.amv1@gmail.com>
2021-11-20 23:55:19 +0100

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 
 function Quote() {
-  const newPage = () =>
-    (window.location.href = `https://twitter.com/intent/tweet?text=${quoteText}`);
-
-  const [animeText, setAnimeText] = useState("");
-  const [quoteText, setQuoteText] = useState("");
-  const [characterText, setCharacterText] = useState("");
+  const newPage = () => (window.location.href = `https://twitter.com/intent/tweet?text=${quotes.quoteText}`);
+  
   const [error, setError] = useState("");
-
+  const [quotes, setQuotes] = useState({
+    animeText: "",
+    quoteText: "",
+    characterText: "",
+  });
   const animeQuote = async () => {
     try {
       const res = await fetch("https://animechan.vercel.app/api/random");
       const random = await res.json();
 
-      setAnimeText(`Anime - ${random.anime}`);
-      setQuoteText(`Quote - ${random.quote}`);
-      setCharacterText(`Character - ${random.character}`);
+      setQuotes({
+        animeText: `Anime - ${random.anime}`,
+        quoteText: `Quote - ${random.quote}`,
+        characterText: `Character - ${random.character}`,
+      });
     } catch (err) {
       setError("Something went wrong");
     }
@@ -26,9 +28,9 @@ function Quote() {
       <div className="results-container">
         <div className="title-container">
           <h3 className="error">{error}</h3>
-          <p>{animeText}</p>
-          <p>{quoteText}</p>
-          <p>{characterText}</p>
+          <p>{quotes.animeText}</p>
+          <p>{quotes.quoteText}</p>
+          <p>{quotes.characterText}</p>
         </div>
         <button className="btn btn-danger" onClick={animeQuote}>
           Generate
