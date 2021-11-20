d69681bd272cfc3e33fb08c224a0e570d3b81454
YuukaDev <vule.amv1@gmail.com>
2021-11-20 23:58:49 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 function Header() {
-    const spanStyle = {
-        color: 'red'
-    }
+  const spanStyle = {
+    color: "red",
+  };
   return (
-      <div className="header-container">
-          <h1>Anime <span style={spanStyle}>Quotes</span></h1>
-          <p>Click On The Button To Generate Random Anime Quote</p>
-      </div>
+    <div className="header-container">
+      <h1>
+        Anime <span style={spanStyle}>Quotes</span>
+      </h1>
+      <p>Click On The Button To Generate Random Anime Quote</p>
+    </div>
   );
 }
 
