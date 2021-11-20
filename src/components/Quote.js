4189fb55d9305a6178852fa4ade21974cd65bd86
YuukaDev <vule.amv1@gmail.com>
2021-11-20 22:50:16 +0100

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 
 function Quote() {
-  const newPage = () => (window.location.href = `https://twitter.com/intent/tweet?text=${quoteText}`);
+  const newPage = () =>
+    (window.location.href = `https://twitter.com/intent/tweet?text=${quoteText}`);
 
   const [animeText, setAnimeText] = useState("");
   const [quoteText, setQuoteText] = useState("");
   const [characterText, setCharacterText] = useState("");
+  const [error, setError] = useState("");
 
   const animeQuote = async () => {
-    const res = await fetch("https://animechan.vercel.app/api/random");
-    const random = await res.json();
+    try {
+      const res = await fetch("https://animechan.vercel.app/api/random");
+      const random = await res.json();
 
-    setAnimeText(`Anime - ${random.anime}`);
-    setQuoteText(`Quote - ${random.quote}`);
-    setCharacterText(`Character - ${random.character}`);
+      setAnimeText(`Anime - ${random.anime}`);
+      setQuoteText(`Quote - ${random.quote}`);
+      setCharacterText(`Character - ${random.character}`);
+    } catch (err) {
+      setError("Something went wrong");
+    }
   };
   return (
     <main>
       <div className="results-container">
         <div className="title-container">
+          <h3 className="error">{error}</h3>
           <p>{animeText}</p>
           <p>{quoteText}</p>
           <p>{characterText}</p>
         </div>
-        <center>
-          <button className="btn btn-danger" onClick={animeQuote}>
-            Generate
-          </button>
-          <button id="tweet" className="btn btn-primary" onClick={newPage}>
-            Tweet
-          </button>
-        </center>
+        <button className="btn btn-danger" onClick={animeQuote}>
+          Generate
+        </button>
+        <button id="tweet" className="btn btn-primary" onClick={newPage}>
+          Tweet
+        </button>
       </div>
     </main>
   );
